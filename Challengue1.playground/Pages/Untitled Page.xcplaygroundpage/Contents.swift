f9aecb12598e997c6cefa642591e9f8a49a44f04
Ayax <casarrubias2837@gmail.com>
2020-11-24 23:24:28 -0600

    Initial commit

---
+++ Challengue1.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+
+func challenge1(input: String) -> Bool {
+    
+    var text = input
+    var array = [Character]()
+    //print(input.count)
+    
+    for _ in 0...input.count {
+        
+        guard let sec = text.popLast() else{
+            break
+        }
+        array.append(sec)
+    }
+    let set = Set(array)
+    if(set.count != input.count) {
+        return false
+    } else {
+        return true
+    }
+    
+}
+
+assert(challenge1(input: "No duplicates") == true, "Challenge 1 failed")
+assert(challenge1(input: "abcdefghijklmnopqrstuvwxyz") == true, "Challenge 1 failed")
+assert(challenge1(input: "AaBbCc") == true, "Challenge 1 failed")
+assert(challenge1(input: "Hello, world") == false, "Challenge 1 failed")
+
+
+//--Optimal solution
+/*
+ 
+func challenge2(input: String) -> Bool {
+    return Set(input).count == input.count
+ }
+ 
+*/
