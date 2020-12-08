33960599342eb71c4acd703cb1e151b08434edba
Ayax <casarrubias2837@gmail.com>
2020-12-07 20:22:17 -0600

    Challenge17

---
+++ Challenge17.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+import Foundation
+
+func challenge17(_ min: Int, _ max: Int) -> Int {
+    return Int.random(in: min...max)
+}
+
+challenge17(1, 5)
+challenge17(8, 10)
+challenge17(12, 12)
+challenge17(12, 18)
+
